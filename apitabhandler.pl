a8fda63ed69b4c4bc216b20fb2735906a03b962d
cP_Sky <10bserver8@gmail.com>
2017-04-01 04:39:54 -0600

    Update apitabhandler.pl

--- apitabhandler.pl
+++ apitabhandler.pl
@@ -1,49 +1,46 @@
 #!/usr/bin/perl
+use strict;
 
-foreach $arg (@ARGV) { $args = $args . " " . $arg; }
+my $args;
+my $docsURL;
 my $apiTabbed = @ARGV[0];
 
+foreach my $arg (@ARGV) { $args = $args . " " . $arg; }
+
+my $apiFullList = "/root/.cpanel/fullset" . $apiTabbed . ".list";
 
 if ( $apiTabbed =~ "uapi" ) {
-     $docsURL="  ## Documentation -> https://documentation.cpanel.net/display/SDK/UAPI+Functions+-+";
-} else {
+    $docsURL =
+"  ## Documentation -> https://documentation.cpanel.net/display/SDK/UAPI+Functions+-+";
+}
+else {
     if ( $apiTabbed =~ "cpapi2" ) {
-        $docsURL="  ## Documentation -> https://documentation.cpanel.net/display/SDK/cPanel+API+2+Functions+-+";
-    } else {
-        $docsURL="  ## Documentation -> https://documentation.cpanel.net/display/SDK/WHM+API+1+Functions+-+";
+        $docsURL =
+"  ## Documentation -> https://documentation.cpanel.net/display/SDK/cPanel+API+2+Functions+-+";
+    }
+    else {
+        $docsURL =
+"  ## Documentation -> https://documentation.cpanel.net/display/SDK/WHM+API+1+Functions+-+";
     }
 }
 
-$apiFuncList = "/root/.cpanel/" . $apiTabbed . ".list";
-$apiFullList = "/root/.cpanel/fullset" . $apiTabbed . ".list";
-
 sub doTabApi {
-    
-    open( INPUTFILE, $apiFuncList ) or die "$!";
-    @lines = <INPUTFILE>;
-    close(INPUTFILE);
     if ( $args =~ /.*cur=(.*)$/ ) {
         my $cur = $1;
-        foreach my $line (@lines) {
-            if ( $line =~ /^$cur.*/ ) {
-                if ( $line =~ m/^$cur$/ ) {
-                       open( FULLINPUTFILE, $apiFullList ) or die "$!";
-                      @fullLines = <FULLINPUTFILE>;
-                      close(FULLINPUTFILE);
-                      foreach my $fullLine (@fullLines) {
-                        if ( $fullLine =~ m/^$cur$/ )  {
-			   if ( $apiTabbed == "whmapi1" ) {
-                            print "$cur" . $docsURL . "$cur";
-			  }
-                        }  else {
-                           $cur =~ s/-/ /;
-                           if ( $fullLine =~ /^$cur.*/ ) {
-                           chomp($fullLine);
-			   $cur =~ s/ /%3A%3A/;
-                           print " --user=\$userName $fullLine" . $docsURL . "$cur";
-                              }
-                          }
-                     }
+        open( FULLINPUTFILE, $apiFullList ) or die "$!";
+        my @fullLines = <FULLINPUTFILE>;
+        close(FULLINPUTFILE);
+        foreach my $fullLine (@fullLines) {
+            if ( $apiTabbed == "whmapi1" && $fullLine =~ /^$cur$/ ) {
+                print $cur . $docsURL . $cur;
+            }
+            else {
+                $cur =~ s/-/\ /;
+                my $holder = $cur;
+                $holder =~ s/ /%3A%3A/;
+                if ( $fullLine =~ /^$cur\s/ ) {
+                    chomp($fullLine);
+                    print " --user=\$userName $fullLine" . $docsURL . $holder;
                 }
             }
         }
